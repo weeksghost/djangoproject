f36dbfae1c3f4d4273f888378019dc37efd6f1bb
Erik Marty <emarty@broadway.com>
2014-03-18 16:52:32 -0400

    Updated backend logic for homepage and added timezone fix for linux boxes

--- homepage/views.py
+++ homepage/views.py
@@ -1,33 +1,24 @@
 from django.shortcuts import render
 
-from homepage.models import FeaturedText, HomeWidget, FeaturedVideo
+from homepage.models import FeaturedText, FeaturedVideo
 from blog.models import Post
 
 def homepage(request):
-    # For main text on homepage
-    featured_text = FeaturedText.objects.all()
-    featured = featured_text.order_by('featured_title', 'featured_subtitle', 'content', 'call_to_action').filter(active=True)[:1]
+    featured = FeaturedText.objects.all().order_by('featured_title', 'featured_subtitle', 'content', 'call_to_action').filter(active=True)
 
     try:
         featured
     except:
         pass
 
-    # For blog entries in home widgets
     posts = Post.objects.all().filter(published=True)
-    # Had to do this becuase of original grid layout
-    third_post = Post.objects.order_by('created').filter(published=True)
-
-    # For video on homepage
-    video = FeaturedVideo.objects.all()
-    extvideo = video.order_by('title', 'embed_url').filter(active=True)[:1]
+    extvideo = FeaturedVideo.objects.all().order_by('title', 'embed_url').filter(active=True)
 
     template = 'homepage.html'
 
     context = {
         'featured':featured,
         'posts': posts,
-        'third': third_post,
         'embed':extvideo,
     }
     return render(request, template, context)
